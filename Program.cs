1290231e078cc775306962ebf13a024960911263
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-27 22:37:06 +0300

    написал рабочий код

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] arr = new string[7] { "Понедельник", "Вторник", "Среда", "Четверег", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine("Введите число, которое взбредет вам в голову от 1 до 7");
+int n = int.Parse(Console.ReadLine());
+
+if (n<1 || n>7)
+{
+Console.WriteLine("Ввведено неверное число");
+
+}
+else
+{
+Console.WriteLine($"{n} - это {arr[n-1]}");
+}
